3e53b08cc53851953d0dfb5eb981fea516f3e13c
friikjones <d.mullerjones@gmail.com>
2025-02-18 12:40:41 -0800

    Added GameManagerScript.cs and Gameplay.unity

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // public bool plant1Selected;
+    public GameObject prefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
