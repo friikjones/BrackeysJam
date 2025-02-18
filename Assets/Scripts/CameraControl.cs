9b9a317f90e7fd8259f14eecbbd29fd765a0dbf1
friikjones <d.mullerjones@gmail.com>
2025-02-17 18:34:47 -0800

    Added square terrarium and light controls

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,14 +3,17 @@ using UnityEngine.Animations;
 
 public class CameraControl : MonoBehaviour
 {
-    public float cameraSpeed, cameraFinesse;
+    public float cameraSpeed, rotationFinesse, zoomFinesse;
     public float maxRotation, minRotation;
+    public float maxDist, minDist;
     private float targetRotation = 180;
+    private float targetDist = -10;
+    private Transform cameraPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        cameraPosition = transform.GetChild(0);
     }
 
     // Update is called once per frame
@@ -25,17 +28,29 @@ public class CameraControl : MonoBehaviour
         if (Input.GetKey(KeyCode.Q))
         {
             if (targetRotation < maxRotation)
-                targetRotation += cameraFinesse;
+                targetRotation += rotationFinesse;
         }
         if (Input.GetKey(KeyCode.E))
         {
             if (targetRotation > minRotation)
-                targetRotation -= cameraFinesse;
+                targetRotation -= rotationFinesse;
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (targetDist > maxDist)
+                targetDist -= zoomFinesse;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (targetDist < minDist)
+                targetDist += zoomFinesse;
 
         }
         // Debug.Log("target: " + targetRotation);
         // Debug.Log("current: " + transform.rotation.eulerAngles);
         transform.localRotation = Quaternion.Euler(Vector3.Lerp(transform.localRotation.eulerAngles, new Vector3(0, targetRotation, 0), Time.deltaTime * cameraSpeed));
+        cameraPosition.localPosition = Vector3.Lerp(cameraPosition.localPosition, new Vector3(0, cameraPosition.localPosition.y, targetDist), Time.deltaTime * cameraSpeed);
 
     }
 }
--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    public float humidity;
+    public float humidity, lightStrength;
+    public Light mainLight;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,15 +13,26 @@ public class PlayerControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.I))
         {
             humidity++;
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.K))
         {
             humidity--;
         }
+        if (Input.GetKey(KeyCode.J))
+        {
+            lightStrength++;
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            lightStrength--;
+        }
         humidity = Mathf.Clamp(humidity, 0, 100);
+        lightStrength = Mathf.Clamp(lightStrength, 0, 100);
+
+        mainLight.intensity = lightStrength / 10;
 
     }
 }
