2a225962713551c6385881b360f6a8770f429aa1
Goulartvic <goulart.vic@gmail.com>
2025-02-18 21:22:40 -0800

    Create raycast to click at selected objects and clone inside the terrarium

---
+++ Assets/Scripts/ClickRaycast.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickRaycast : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    public GameObject[] cloneableObjects; 
+
+    private GameObject selectedObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject clickedObject = hit.collider.gameObject;
+                if (System.Array.Exists(cloneableObjects, obj => obj == clickedObject))
+                {
+                    selectedObject = clickedObject;
+                    Debug.Log("Selected object: " + selectedObject.name);
+                }
+
+                else if (hit.collider.CompareTag("Plane") && selectedObject != null)
+                {
+                    Instantiate(selectedObject, hit.point, Quaternion.identity);
+                    Debug.Log("Created object at: " + hit.point);
+                }
+            }
+        }
+    }
+}
